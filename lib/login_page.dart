e2366a02badf46bb44dc3b5d4d5957330aa9d617
sanshanthan <sanshanthan@yahoo.com>
2023-05-04 02:35:24 +0530

    column replaced by SingleChildScrollView

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,114 +8,106 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 45.0,
-          ),
-          const Image(
-            image: AssetImage("images/logo.png"),
-            width: 250.0,
-            height: 250.0,
-            alignment: Alignment.center,
-          ),
-          const SizedBox(
-            height: 1.0,
-          ),
-          const Text(
-            "Login as a Rider",
-            style: TextStyle(fontSize: 24.0),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          TextButton(
-            style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 10),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 45.0,
             ),
-            onPressed: () {
-              if (kDebugMode) {
-                print("clicked");
-              }
-            },
-            child: const Text('Do not have an account? Register Here.'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: const [
-                SizedBox(
-                  height: 1.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    labelStyle: TextStyle(
-                      fontSize: 14.0,
-                    ),
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 10.0,
-                    ),
+            const Image(
+              image: AssetImage("images/logo.png"),
+              width: 300.0,
+              height: 300.0,
+              alignment: Alignment.center,
+            ),
+            const SizedBox(
+              height: 1.0,
+            ),
+            const Text(
+              "Login as a Rider",
+              style: TextStyle(fontSize: 24.0),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: const [
+                  SizedBox(
+                    height: 1.0,
                   ),
-                  style: TextStyle(fontSize: 14.0),
-                ),
-                SizedBox(
-                  height: 1.0,
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: TextStyle(
-                      fontSize: 14.0,
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0,
+                      ),
                     ),
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 10.0,
-                    ),
+                    style: TextStyle(fontSize: 14.0),
                   ),
-                  style: TextStyle(fontSize: 14.0),
-                ),
-              ],
+                  SizedBox(
+                    height: 1.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 1.0,
-          ),
-          ElevatedButton(
-            // color: Colors.yellow,
-            // textColor: Colors.white,
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.yellow, // Background color
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.0)),
+            const SizedBox(
+              height: 1.0,
             ),
-            child: const SizedBox(
-              height: 50.0,
-              child: Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontSize: 18.0, color: Colors.black),
+            ElevatedButton(
+              // color: Colors.yellow,
+              // textColor: Colors.white,
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow, // Background color
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0)),
+              ),
+              child: const SizedBox(
+                height: 50.0,
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 18.0, color: Colors.black),
+                  ),
                 ),
               ),
             ),
-          ),
-          // const SizedBox(height: 10),
-          // TextButton(
-          //   style: TextButton.styleFrom(
-          //     textStyle: const TextStyle(fontSize: 10),
-          //   ),
-          //   onPressed: () {
-          //     if (kDebugMode) {
-          //       print("clicked");
-          //     }
-          //   },
-          //   child: const Text('Do not have an account? Register Here.'),
-          // ),
-        ],
-      ),
+            const SizedBox(height: 10),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 15),
+              ),
+              onPressed: () {
+                if (kDebugMode) {
+                  print("clicked");
+                }
+              },
+              child: const Text('Do not have an account? Register Here.'),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
